de8c91eb1941e3993f01cdc995207344b9d65ce0
dibyaranjan <dibyaranjan@propertyguru.com.sg>
2020-11-29 20:04:55 +0530

    removed aws for env variable

--- api/route/s3data.js
+++ api/route/s3data.js
@@ -2,8 +2,8 @@
 const AWS = require('aws-sdk');
 
 const s3data = new AWS.S3({
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET
+    accessKeyId: process.env.ID,
+    secretAccessKey: process.env.SECRET
 });
 
 exports.model = s3data;
