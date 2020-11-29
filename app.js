33e98d2ed57e5fed28cf0019ab2c731895159a16
dibyaranjan-pg <58494847+dibyaranjan-pg@users.noreply.github.com>
2020-11-29 14:42:09 +0530

    Update app.js
    updated user name and pwd with env variable

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const listItem = require('./api/route/listitem');
-const uri = "mongodb+srv://@qafficient.leqm1.mongodb.net/rentaway-db?retryWrites=true&w=majority";
+const listItem = require("./api/route/listitem");
+const uri =
+  "mongodb+srv://" +
+  process.env.RENT_AWAY_DB_UN +
+  ":" +
+  process.env.RENT_AWAY_DB +
+  "@qafficient.leqm1.mongodb.net/rentaway-db?retryWrites=true&w=majority";
 mongoose.connect(uri);
 
-require('dotenv').config()
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/listitem',listItem);
+app.use("/listitem", listItem);
 
 module.exports = app;
-
