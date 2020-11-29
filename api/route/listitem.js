d1852d5c2e7d8e128d5dea4515bc31eb8981f1cd
dibyaranjan <dibyaranjan@propertyguru.com.sg>
2020-11-29 14:40:17 +0530

    fixed issue with posting item with media entry

--- api/route/listitem.js
+++ api/route/listitem.js
@@ -36,52 +36,77 @@ router.post("/", upload, (req, res, next) => {
   item
     .save()
     .then((result) => {
-        let itemId = result.id;
-        console.log(itemId);
-
-      // upload image to s3
-      let imgFiles = req.files;
-      imgFiles.forEach(function (imgItem, index, array) {
-        
-        
-        console.log("**************** for each called ******************");
-        imgItems = imgItem.originalname.split(".");
-        const fileType = imgItems[imgItems.length - 1];
-
-        const params = {
-          Bucket: process.env.AWS_BUCKET_NAME,
-          Key: `${uuidv4()}.${fileType}`,
-          Body: imgItem.buffer,
-        };
-
-        var s3uploadResult =  s3.upload(params).promise();
-        s3uploadResult.then((uploadResult) => {
-
-          console.log(uploadResult.Location);
-
-          //call to create media entry;
-          const media = new Media({
-            _id: new mongoose.Types.ObjectId(),
-            item_id: itemId,
-            location: uploadResult.Location,
-            isCover: true,
-          });
-          media
-            .save()
-            .then((mediaResult) => {
-              console.log(mediaResult);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }).catch(error => console.log(error));
+      let itemId = result.id;
+      multipleUploads(req.files, itemId);
+      res.status(200).json({
+        message: result,
       });
     })
     .catch((error) => {
       console.log(error);
+      res.status(500).json({
+        message: "Could not create item",
+      });
     });
 });
 
-function uploadImageAndMediaEntry(item, index, files, itemId) {}
+function addMediaEntry(imageEntries, itemId){
+    const media = new Media({
+        _id: new mongoose.Types.ObjectId(),
+         item_id: itemId,
+        images: imageEntries,
+      });
+      media
+        .save()
+        .then((mediaResult) => {
+          console.log(mediaResult);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+}
+
+function multipleUploads(files, itemId) { 
+    
+  var promises = [];
+  files.forEach(function (imgItem, index, array) {
+      if(index == 0){
+        promises.push(uploadToS3(imgItem, true));
+      }else{
+        promises.push(uploadToS3(imgItem, false));
+      }
+  });
+  Promise.all(promises).then((imageEntries) => {
+      addMediaEntry(imageEntries, itemId);
+  })
+}
+
+function uploadToS3(imgItem, isCoverImage) {
+    return new Promise( function (resolve, reject) {
+        imgItems = imgItem.originalname.split(".");
+        const fileType = imgItems[imgItems.length - 1];
+        const params = buildParam(imgItem, fileType);       
+        var s3uploadResult = s3.upload(params).promise();
+        s3uploadResult
+            .then((uploadResult) => {
+            console.log(uploadResult.Location);
+            resolve( {
+                location: uploadResult.Location,
+                isCover: isCoverImage,
+            });
+            })
+            .catch((error) => console.log(error));
+            })
+  
+}
+
+function buildParam(imgItem, fileType) {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuidv4()}.${fileType}`,
+    Body: imgItem.buffer,
+  };
+  return params;
+}
 
 module.exports = router;
